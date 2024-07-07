b871257960d55ba84b0612baa072439d4cfc45e9
Samuel Mascarenhas <samuelmascarenhascontato@gmail.com>
2024-07-07 20:10:25 -0300

    Layout Finalizado

---
+++ src/components/ItemMenu.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+import Arrow from '@/assets/arrow-down.svg'
+import { type } from "os";
+
+type Props = {
+name: string
+}
+
+
+
+export function ItemMenu({name}: Props) {
+    return (
+        <button className=" flex items-center gap-3">
+            <span className="text-white font-bold ">{name}</span>
+
+            <Image
+                src={Arrow}
+                alt="Arrow"
+            />
+        </button>
+
+    )
+}
